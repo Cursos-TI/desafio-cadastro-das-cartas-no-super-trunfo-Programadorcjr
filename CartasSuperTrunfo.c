696625ab930b0e44f5d15577fedbff36c811ccb8
Programadorjr <208865809+Programadorcjr@users.noreply.github.com>
2025-05-09 23:17:47 +0000

    Desafio concluido novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-
-int main() {
-   
-  // Declaração de variavel Primeira carta 
+int main (){
+    // Declaração de variavel Primeira carta 
     char Estado1;
     char Codigo1[2];
     char Cidade1[50];
@@ -46,5 +44,55 @@ int main() {
     printf("Area1: %f\n", Area1);
     printf("PIB1: %f\n", PIB1);
     printf("Pontoturistico1: %d\n", Pontoturistico1);
+
+
+    // Declaração de variavel segunda carta 
+    char Estado2;
+    char Codigo2[2];
+    char Cidade2[50];
+    float População2;
+    float Area2;
+    float PIB2;
+    int Pontoturistico2;
+
+    // Leitura da carta 2
+    printf("\nDigite os dados da carta2\n");
+
+    printf("Estado2(Uma Letra de A a H)\n");
+    scanf("%c", &Estado2);
+
+    printf("Digite o Codigo2(EX: A1, H1)\n");
+    scanf("%s", &Codigo2);
+
+    printf("Digite a Cidade2\n"); 
+    scanf("%s", &Cidade2);
+
+    printf("Didite a População2\n");
+    scanf("%f", &População2);
+
+    printf("Digite a Area2\n");
+    scanf("%f", &Area2);
+
+    printf("Digite o PIB2 em milhões de reais\n");
+    scanf("%f", &PIB2);
+
+    printf("Digite Pontos turisticos2\n");
+    scanf("%d", &Pontoturistico2);
+
+    // Exibição de dados
+    printf("\nDados da Carta2\n");
+
+    printf("Estado2: %c\n", Estado2);
+    printf("Codido2: %s\n", Codigo2);
+    printf("Cidaade2: %s\n", Cidade2);
+    printf("População2: %f\n", População2);
+    printf("Area2: %f\n", Area2);
+    printf("PIB2: %f\n", PIB2);
+    printf("Pontoturistico2: %d\n", Pontoturistico2);
+
+
+
+
     return 0;
+
 }
